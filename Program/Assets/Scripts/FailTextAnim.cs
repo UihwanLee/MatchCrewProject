8dc3171211001549fb8e9375eac4ff82b3c66ab4
Lee Ui Hwan <luh5063014@naver.com>
2025-09-23 12:09:52 +0900

    Feat: 떨어지는 Delay를 랜덤하게 구현

--- Program/Assets/Scripts/FailTextAnim.cs
+++ Program/Assets/Scripts/FailTextAnim.cs
@@ -9,16 +9,22 @@ using UnityEngine.UI;
 public class FailTextAnim : MonoBehaviour
 {
     [SerializeField] private Text[] letters;
-    [SerializeField] private float delay = 0.5f;
+    [SerializeField] private float baseDelay = 0.1f;
+    [SerializeField] private float randomDelay = 0.05f;
     [SerializeField] private float fadeDuration = 1.5f;
     [SerializeField] private float moveDistance = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        float delay = 0f;
+
         // 코루틴을 사용하여 한 글자 씩 내려가는 연출 구현
         for(int i=0; i<letters.Length; i++)
         {
+            float temp = baseDelay + Random.Range(0f, randomDelay);
+            delay += temp;
+
             StartCoroutine(PlayFailAnimation(letters[i], i * delay));
         }
     }
